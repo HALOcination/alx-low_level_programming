97c17bd6a54b1fb2c818e67d0f76d992f4e3a849
Sahar <aguidi53@gmail.com>
2023-04-09 07:53:12 -0700

    0.whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
-{
-	printf("%s\n", *argv);
 
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
