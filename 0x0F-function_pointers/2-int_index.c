ecfaf0332937e802242467d9e7c80d5a660920c9
Sahar <aguidi53@gmail.com>
2023-04-19 09:33:17 -0700

    3-main.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - search for integer
+ * @array: the int array
+ * @size: the size of array
+ * @cmp: function compared
+ *
+ * Return: void
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (array && size && cmp)
+		while (i < size)
+		{
+			if (cmp(array[i]))
+				return (i);
+			i++;
+		}
+	return (-1);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calculats the sum of two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ *Return: the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - calculats the difference of two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the difference of a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - calculats the division of two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: division of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculats the modulus of two integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
