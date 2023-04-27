baa6b1606a8736d301dcf350ea1da40e742a0a8e
Sahar <Aguidi53@gmail.com>
2023-04-27 12:20:38 -0700

    0.print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,6 @@
 #include "lists.h"
 
 /**
-  * _strlen - prints the length of string
-  * @s: the string
-  *
-  * Return: int string length
-  */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	if (!s)
-		return (0);
-	while (*s++)
-		i++;
-	return (i);
-}
-
-/**
  * print_list - prints all the elements of a linked list
  * @h: pointer to print
  *
@@ -29,9 +12,13 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-		h = h->nest;
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 		i++;
 	}
+
 	return (i);
 }
